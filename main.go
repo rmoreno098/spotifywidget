c7382f0c2818df933ee37b77fa8a82a7a22a3095
Luis de Santos <ldesantos1501@gmail.com>
2023-10-04 13:09:10 -0500

    Added test handler

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
+	"time"
+
 	"github.com/gorilla/mux"
 )
 
-func main(){
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Printing from new thread")
+	str := "Hello from go server"
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, str)
+	time.Sleep(5 * time.Second)
+}
+
+func main() {
 	r := mux.NewRouter()
-	
-	r.HandleFunc("/api/hello", func (w http.ResponseWriter, r *http.Request){
-		fmt.Println("Hello, world!")
-	})
+
+	r.HandleFunc("/api/hello", hello)
 
 	http.Handle("/", r)
 	fmt.Println("Server is running on :8080")
--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+const fetchData = async () => {
+  try {
+    const response = await fetch('http://localhost:8080/api/hello');
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      throw new Error('API request failed');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchData}>Click me</button>
       </header>
     </div>
   );
