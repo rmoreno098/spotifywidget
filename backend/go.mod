e59bee9d5563fb278ce22664153bf5dc48f73158
roman <roman.moreno226@live.com>
2023-12-13 13:07:29 -0600

    Switch from CRA to bun

--- backend/go.mod
+++
@@ -1,5 +0,0 @@
-module spotify-widget
-
-go 1.21.1
-
-require github.com/rs/cors v1.10.1
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
