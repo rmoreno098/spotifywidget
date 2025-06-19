12d71f157673ce8e356a3dca882cf9b11c5960ee
Roman Moreno <roman.moreno226@live.com>
2025-06-18 23:18:37 -0500

    Updated PlaylistTracks to get playlist ID from URL instead of body

--- backend/handlers/widget.go
+++ backend/handlers/widget.go
@@ -2,29 +2,24 @@ package handlers
 
 import (
 	"encoding/json"
-	"io"
 	"log/slog"
 	"net/http"
 	"spotify-widget-v2/models"
+
+	"github.com/gorilla/mux"
 )
 
 func (h *Handler) PlaylistTracks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	playlistID := vars["id"]
+	if playlistID == "" {
+		http.Error(w, "PlaylistID not found", http.StatusBadRequest)
+		return
+	}
+
 	session := r.Context().Value(models.AuthContext{Session: "session"}).(*models.Token)
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		slog.Error("Error reading body", "error", err)
-		http.Error(w, "Error reading body", http.StatusBadRequest)
-		return
-	}
 
-	var p models.PlaylistItem
-	if err := json.Unmarshal(body, &p); err != nil {
-		slog.Error("Error unmarshalling body", "error", err)
-		http.Error(w, "Error unmarshalling body", http.StatusBadRequest)
-		return
-	}
-
-	tracks, err := h.Spotify.GetPlaylistTracks(session.AccessToken, p.ID)
+	tracks, err := h.Spotify.GetPlaylistTracks(session.AccessToken, playlistID)
 	if err != nil {
 		slog.Error("Error getting tracks", "error", err)
 		http.Error(w, "Error fetching tracks from Spotify", http.StatusInternalServerError)
